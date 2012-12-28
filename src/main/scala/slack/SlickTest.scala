11b56645215fa130c3a4bad7d81670d2f3f78ee4
Josh Marcus <josh.marcus@gmail.com>
2012-12-28 16:36:49 -0500

    Add signal tracking current time to simple test.

--- src/main/scala/slack/SlickTest.scala
+++ src/main/scala/slack/SlickTest.scala
@@ -1,5 +1,8 @@
 package slack
 
+import scala.react._
+import scala.react.Domain
+import slackdom._
 
 // Initial goal: set up a signal that reports the current time,
 // and show it in a Slick2D app.
@@ -7,17 +10,36 @@ package slack
 import org.newdawn.slick.AppGameContainer
 import org.newdawn.slick.{BasicGame, GameContainer, Graphics, Color}
 
-object SlackTest {
-  def main(args: Array[String]) = {
-    val container = new AppGameContainer(new SlackTest, 800, 600, false)
+object SlackTest extends ReactiveApp {
+  val container = new AppGameContainer(new SlackTest, 800, 600, false)
 
-    container.setTargetFrameRate(20)
-    container.start
+  container.setTargetFrameRate(20)
+  container.start
+}
+
+case class Clock extends EventSource[Int](slackdom.owner) {
+  val sig = Var[String]("")
+  var turnNumber = 0
+  def clockTick {
+    val newDate:String = new java.util.Date().toString
+    sig update newDate
+    turnNumber += 1
   }
 }
 
+case class ConsoleObserver(dateSignal:Var[String]) extends Observing {
+  var currentTime:String = ""
+  schedule { obs } 
+  def obs {
+    observe(dateSignal)(timeSignal => currentTime = timeSignal)
+  }
+}
+
+
 class SlackTest extends BasicGame("Slack Test") {
   var current = new java.util.Date()
+  val clock = Clock()
+  val observer = ConsoleObserver(clock.sig)
 
   override def init(gc: GameContainer) {
     println("Slack test started.")
@@ -26,10 +48,11 @@ class SlackTest extends BasicGame("Slack Test") {
   // Execute a turn per update
   override def update(gc: GameContainer, delta: Int) {
     current = new java.util.Date()
+    clock.clockTick
   }
 
   override def render(gc: GameContainer, g: Graphics) {
     g.setColor(Color.white)
-    g.drawString("Time signal: %s".format(current), 200, 10)
+    g.drawString("Time signal: " + observer.currentTime, 200, 10)
   }
 }

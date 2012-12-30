cf5756059755afb649246460ea309c719593168f
Josh Marcus <josh.marcus@gmail.com>
2012-12-29 22:25:56 -0500

    Remove cp -r option from OSX binary script.

--- pull_osx_binaries.sh
+++ pull_osx_binaries.sh
@@ -1,6 +1,6 @@
 curl -o lwjgl2.8.5.zip https://nodeload.github.com/LWJGL/lwjgl/zip/lwjgl2.8.5
 unzip lwjgl2.8.5.zip
 mkdir -p lib
-cp -ar lwjgl-lwjgl2.8.5/libs/macosx/* lib
+cp -a lwjgl-lwjgl2.8.5/libs/macosx/* lib
 rm -rf  lwjgl-lwjgl2.8.5
 rm lwjgl2.8.5.zip

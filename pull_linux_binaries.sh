ce9f6c9bee8a84143b043477e9f16d3e08a6b512
Josh Marcus <josh.marcus@gmail.com>
2013-01-03 15:36:57 -0500

    Only pull linux binaries.

--- pull_linux_binaries.sh
+++ pull_linux_binaries.sh
@@ -1,7 +1,7 @@
-curl -o lwjgl-2.9.0.zip "http://www.newdawnsoftware.com/jenkins/view/LWJGL/job/LWJGL/lastSuccessfulBuild/artifact/dist/lwjgl-2.9.0.zip"
-unzip -o lwjgl-2.9.0.zip
+wget -O lwjgl-2.8.5.zip "http://downloads.sourceforge.net/project/java-game-lib/Official%20Releases/LWJGL%202.8.5/lwjgl-2.8.5.zip?r=&ts=1357241147&use_mirror=iweb"
+unzip -o lwjgl-2.8.5.zip
 mkdir -p lib
-cp -a lwjgl-2.9.0/jar/*  lib
-cp -a lwjgl-2.9.0/native/linux/*  lib
-#rm -rf  lwjgl-2.9.0
-#rm lwjgl-2.9.0.zip
+#cp -a lwjgl-2.8.5/jar/*  lib
+cp -a lwjgl-2.8.5/native/linux/*  lib
+rm -rf  lwjgl-2.8.5
+rm lwjgl-2.8.5.zip
